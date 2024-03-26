beaeff642a0bd10a5c56a755e99bf2226476f1d6
DUONGAN <roadtofinishwebsite@gmail.com>
2024-03-26 11:58:15 +0700

    add domain entities for project

---
+++ TeduSop.Model/Models/VisitorStatistic.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduSop.Model.Models
+{
+    class VisitorStatistic
+    {
+    }
+}
