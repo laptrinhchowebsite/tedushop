1de8f9422ea958e2472a94036234c81ec6ec53f3
DUONGAN <roadtofinishwebsite@gmail.com>
2024-03-31 10:52:40 +0700

    add unit test for repository

---
+++ Tedu.UnitTest/RepositoryTest/PostCategoryRepositoryTest.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TeduShop.Data.Infrastructure;
+using TeduShop.Data.Repositories;
+using TeduSop.Model.Models;
+using System.Linq;
+
+namespace Tedu.UnitTest.RepositoryTest
+{
+    [TestClass]
+    public class PostCategoryRepositoryTest
+    {
+        IDbFactory dbFactory;
+        IPostCategoryRepository objRepository;
+        IUnitOfWork unitOfWork;
+        [TestInitialize]
+        public void Initialize()
+        {
+            dbFactory = new DbFactory();
+            objRepository = new PostCategoryRepository(dbFactory);
+            unitOfWork = new UnitOfWork(dbFactory);
+
+        }
+        [TestMethod]
+        public void PostCategory_Repository_Create()
+        {
+            PostCategory category = new PostCategory();
+            category.Name = "Test Category";
+            category.Alias = "Test Category";
+            category.Status = true;
+
+            var result = objRepository.Add(category);
+            unitOfWork.Commit();
+
+            Assert.AreEqual(5, result.ID);
+            Assert.IsNotNull(result);
+        }
+        [TestMethod]
+        public void PostCategory_Repository_GetAll()
+        {
+            var list = objRepository.GetAll();
+            Assert.AreEqual(5,list.Count());
+        }
+    }
+}
--- TeduShop.Service/PostCategoryService.cs
+++ TeduShop.Service/PostCategoryService.cs
@@ -7,17 +7,19 @@ namespace TeduShop.Service
 {
     public interface IPostCategoryService
     {
-        void Add(PostCategory postCategory);
+        PostCategory Add(PostCategory postCategory);
 
         void Update(PostCategory postCategory);
 
-        void Delete(int id);
+        PostCategory Delete(int id);
 
         IEnumerable<PostCategory> GetAll();
 
         IEnumerable<PostCategory> GetAllByParent(int parentId);
 
         PostCategory GetById(int id);
+        void Save();
+
     }
 
     public class PostCategoryService : IPostCategoryService
@@ -30,15 +32,14 @@ namespace TeduShop.Service
             this._postCategoryRepository = postCategoryRepository;
             this._unitOfWork = unitOfWork;
         }
-
-        public void Add(PostCategory postCategory)
+        public PostCategory Add(PostCategory postCategory)
         {
-            _postCategoryRepository.Add(postCategory);
+            return _postCategoryRepository.Add(postCategory);
         }
 
-        public void Delete(int id)
+        public PostCategory Delete(int id)
         {
-            _postCategoryRepository.Delete(id);
+            return _postCategoryRepository.Delete(id);
         }
 
         public IEnumerable<PostCategory> GetAll()
@@ -55,7 +56,10 @@ namespace TeduShop.Service
         {
             return _postCategoryRepository.GetSingleById(id);
         }
-
+        public void Save()
+        {
+            _unitOfWork.Commit();
+        }
         public void Update(PostCategory postCategory)
         {
             _postCategoryRepository.Update(postCategory);
