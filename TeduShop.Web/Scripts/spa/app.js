64fc349b8f5433ae8a8428c26df49357711dba05
DUONGAN <roadtofinishwebsite@gmail.com>
2024-04-05 17:04:03 +0700

    service, controller angular 17-19

---
+++ TeduShop.Web/Scripts/spa/app.js
@@ -0,0 +1,34 @@
+﻿/// <reference path="../plugins/angular/angular.js" />
+
+var myApp = angular.module('myModule', []);
+myApp.controller("schoolController", schoolController);
+myApp.service("Validator", Validator);
+
+schoolController.$inject = ['$scope','Validator']
+//declare
+function schoolController($scope, Validator) {
+   
+    Validator.checkNumber(1);
+    $scope.checkNumber = function ()
+    {
+        $scope.message = Validator.checkNumber($scope.num);
+    }
+    $scope.num = 1;
+}
+
+function Validator($window)
+{
+    return {
+        checkNumber: checkNumber
+    }
+    function checkNumber(input)
+    {
+        if (input % 2 == 0) {
+            return  'This is even';
+        }
+        else
+            return 'This is odd';
+
+    }
+
+}
