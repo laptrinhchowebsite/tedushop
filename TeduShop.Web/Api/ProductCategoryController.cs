c5bb578825d1089609b36c04aa205b4c5580e796
DUONGAN <roadtofinishwebsite@gmail.com>
2024-04-14 13:08:28 +0700

    Cập nhật dữ liệu trong Angular JS

--- TeduShop.Web/Api/ProductCategoryController.cs
+++ TeduShop.Web/Api/ProductCategoryController.cs
@@ -16,12 +16,15 @@ namespace TeduShop.Web.Api
     [RoutePrefix("api/productcategory")]
     public class ProductCategoryController : ApiControllerBase
     {
+        #region Initialize
         IProductCategoryService _productCategoryService;
         public ProductCategoryController(IErrorService errorService,IProductCategoryService productCategoryService) 
             : base(errorService)
         {
             this._productCategoryService = productCategoryService;
         }
+        #endregion
+
         [Route("getallparents")]
         [HttpGet]
         public HttpResponseMessage GetAll(HttpRequestMessage request)
@@ -78,6 +81,7 @@ namespace TeduShop.Web.Api
                  {
                      var newProductCategory = new ProductCategory();
                      newProductCategory.UpdateProductCategory(productCatrgoryVm);
+                     newProductCategory.CreatedDate = DateTime.Now;
                      _productCategoryService.Add(newProductCategory);
                      _productCategoryService.Save();
 
@@ -88,6 +92,50 @@ namespace TeduShop.Web.Api
                  return response;
              });
 
+        } 
+        [Route("getbyid/{id:int}")]
+        [HttpGet]       
+        public HttpResponseMessage GetById(HttpRequestMessage request,int id)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                var model = _productCategoryService.GetById(id);
+
+                var responseData = Mapper.Map<ProductCategory, ProductCategoryViewModel>(model);
+
+                var response = request.CreateResponse(HttpStatusCode.OK, responseData);
+                return response;
+            });
         }
+
+        [Route("update")]
+        [HttpPut]
+        [AllowAnonymous]
+        public HttpResponseMessage Update(HttpRequestMessage request, ProductCategoryViewModel productCatrgoryVm)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                HttpResponseMessage response = null;
+                if (!ModelState.IsValid)
+                {
+                    response = request.CreateResponse(HttpStatusCode.BadRequest, ModelState);
+                }
+                else
+                {
+                    var dbProductCategory = _productCategoryService.GetById(productCatrgoryVm.ID);
+                    dbProductCategory.UpdateProductCategory(productCatrgoryVm);
+                    dbProductCategory.UpdatedDate = DateTime.Now;
+                    _productCategoryService.Update(dbProductCategory);
+                    _productCategoryService.Save();
+
+                    var responseData = Mapper.Map<ProductCategory, ProductCategoryViewModel>(dbProductCategory);
+                    response = request.CreateResponse(HttpStatusCode.Created, responseData);
+                }
+
+                return response;
+            });
+
+        }
+
     }
 }
