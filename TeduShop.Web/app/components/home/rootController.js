0278acd051499318e962f930b8e8d8eb4c825b88
DUONGAN <roadtofinishwebsite@gmail.com>
2024-04-19 23:17:10 +0700

    login form

---
+++ TeduShop.Web/app/components/home/rootController.js
@@ -0,0 +1,15 @@
+﻿(function (app) {
+    app.controller('rootController', rootController);
+
+    rootController.$inject = ['$state', '$scope'];
+
+    function rootController($state, $scope) {
+        $scope.logOut = function () {
+            //loginService.logOut();
+            $state.go('login');
+        }
+        //$scope.authentication = authData.authenticationData;
+        //$scope.sideBar = "/app/shared/views/sideBar.html";
+        //authenticationService.validateRequest();
+    }
+})(angular.module('tedushop'));
