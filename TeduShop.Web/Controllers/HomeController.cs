4ed73c09c18ae3f359c580b495ba5f6c3fe0e306
DUONGAN <roadtofinishwebsite@gmail.com>
2024-04-24 10:54:30 +0700

    OutCache

--- TeduShop.Web/Controllers/HomeController.cs
+++ TeduShop.Web/Controllers/HomeController.cs
@@ -19,7 +19,7 @@ namespace TeduShop.Web.Controllers
             this._commonService = commonService;
             this._productService = productService;
         }
-
+        [OutputCache(Duration = 60,Location = System.Web.UI.OutputCacheLocation.Server)]
         public ActionResult Index()
         {
             var slideModel = _commonService.GetSlides();
@@ -38,21 +38,8 @@ namespace TeduShop.Web.Controllers
             return View(homeViewModel);
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-
         [ChildActionOnly]
+        [OutputCache(Duration =3600)]
         public ActionResult Footer()
         {
             var footerModel = _commonService.GetFooter();
@@ -67,6 +54,8 @@ namespace TeduShop.Web.Controllers
         }
 
         [ChildActionOnly]
+        [OutputCache(Duration = 3600)]
+
         public ActionResult Category()
         {
             var model = _productCategoryService.GetAll();
